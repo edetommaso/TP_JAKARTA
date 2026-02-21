f7b34c323c448a1a960f0c5492d287ce9f059ae6
Eric <eric.detommaso@gmail.com>
2026-02-21 19:37:13 +0100

    TP4 init + Ex 1

---
+++ src/main/java/com/example/tpjakarta/MasterAnnonceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpjakarta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterAnnonceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterAnnonceApplication.class, args);
+    }
+
+}
